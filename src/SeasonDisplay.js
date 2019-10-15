7f763f6d7af840075b0a8e3b785b2e92fb59b625
Fellipe <fontan.fellipe@gmail.com>
2019-10-15 08:38:05 -0300

    first commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonDisplay = (props) => {
+    return (
+        <div>Season Display</div>
+    );
+};
+
+export default SeasonDisplay;
